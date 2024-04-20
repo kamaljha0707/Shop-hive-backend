ea4f9ca9d22e9f99583d33f5bde030ae6c5cec81
userkamaljha <userkamaljha@gmail.com>
2024-04-20 15:57:56 +0530

    app.js file update

--- src/app.js
+++ src/app.js
@@ -1,51 +1,57 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import nodemailer from "nodemailer"
-import path from 'path'
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
+// Import routes
+import productRouter from "./routes/product.routes.js";
+import brandRouter from "./routes/brand.routes.js";
+import categoryRouter from "./routes/category.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import cartRouter from "./routes/cart.routes.js";
+import orderRouter from "./routes/order.routes.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+// Enable CORS
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true,
-    exposedHeaders:['X-Total-Count']
-}))
-app.use(cookieParser())
-app.use(express.json({limit:'16kb'}))
-app.use(express.urlencoded({extended: true, limit: '16kb'}))
-app.use(express.static(path.join( 'dist')));
+    credentials: true,
+    exposedHeaders: ['X-Total-Count']
+}));
 
-// app.get('*', (req, res) =>
-//   res.sendFile(path.resolve('dist', 'index.html'))
-// );
+// Parse cookies
+app.use(cookieParser());
 
+// Parse JSON requests
+app.use(express.json({ limit: '16kb' }));
 
+// Parse URL-encoded requests
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
-// routes import
-import productRouter from './routes/product.routes.js'
-import brandRouter from './routes/brand.routes.js'
-import categoryRouter from './routes/category.routes.js'
-import authRouter from "./routes/auth.routes.js"
-import userRouter from "./routes/user.routes.js"
-import cartRouter from "./routes/cart.routes.js"
-import orderRouter from "./routes/order.routes.js"
+// Use routers for specific paths
+app.use("/", productRouter);
+app.use("/brands", brandRouter);
+app.use("/categories", categoryRouter);
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/cart", cartRouter);
+app.use("/orders", orderRouter);
 
+// Serve index.html for all other routes
+app.get('/*', (req, res) => {
+  const indexPath = path.resolve(__dirname, '..', 'dist', 'index.html');
+  res.sendFile(indexPath);
+});
 
-
-//declration
-
- app.use("/", productRouter)
- app.use("/brands", brandRouter)
- app.use("/categories", categoryRouter)
- app.use("/auth", authRouter)
- app.use("/user", userRouter)
- app.use("/cart", cartRouter)
- app.use("/orders", orderRouter)
-
-
-
-
-export {app}
-
-
+export { app };
